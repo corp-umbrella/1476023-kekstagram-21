15b2da626046d9133c601b63643e44323eacc47e
Иван <vanyastepanow@yandex.ru>
2020-10-10 01:02:10 +0300

    Исправляют остальные ошибки в задании

--- js/main.js
+++ js/main.js
@@ -24,34 +24,34 @@ const getLikesNumber = function () {
 
 const getMessage = function () {
   const allMessages = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+    `Всё отлично!`,
+    `В целом всё неплохо. Но не всё.`,
+    `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+    `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+    `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+    `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
   ];
   return allMessages[getRandomNumber(1, allMessages.length)];
 };
 
 const getName = function () {
   const allNames = [
-    'Пётр',
-    'Елена',
-    'Владимир',
-    'Алексей',
-    'Анастасия',
-    'Владислав',
-    'Евгений',
-    'Анжела',
-    'Илья',
-    'Стас',
-    'Борис',
-    'Фёдор',
-    'Олег',
-    'Мария',
-    'Екатерина',
-    'Полина'
+    `Пётр`,
+    `Елена`,
+    `Владимир`,
+    `Алексей`,
+    `Анастасия`,
+    `Владислав`,
+    `Евгений`,
+    `Анжела`,
+    `Илья`,
+    `Стас`,
+    `Борис`,
+    `Фёдор`,
+    `Олег`,
+    `Мария`,
+    `Екатерина`,
+    `Полина`
   ];
 
   return allNames[getRandomNumber(1, allNames.length)];
@@ -92,18 +92,18 @@ const getPhotos = function () {
 
 const photosList = getPhotos();
 
-const pictureList = document.querySelector('.pictures');
+const pictureList = document.querySelector(`.pictures`);
 
-const picture = document.querySelector('#picture').content;
-const pictureElement = picture.querySelector('.picture');
+const picture = document.querySelector(`#picture`).content;
+const pictureElement = picture.querySelector(`.picture`);
 
-for (var i = 0; i < PHOTOS_AMOUNT; i++) {
+for (let i = 0; i < PHOTOS_AMOUNT; i++) {
   const clonedElement = pictureElement.cloneNode(true);
 
-  const pictureImage = clonedElement.querySelector('.picture__img');
-  // const pictureDescription = clonedElement.querySelector('.picture__info');
-  const pictureComments = clonedElement.querySelector('.picture__comments');
-  const pictureLikes = clonedElement.querySelector('.picture__likes');
+  const pictureImage = clonedElement.querySelector(`.picture__img`);
+  // const pictureDescription = clonedElement.querySelector(`.picture__info`);
+  const pictureComments = clonedElement.querySelector(`.picture__comments`);
+  const pictureLikes = clonedElement.querySelector(`.picture__likes`);
 
   pictureImage.src = photosList[i].url;
   pictureImage.alt = photosList[i].description;
