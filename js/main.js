54a5b524a1899d4fa10ff785943024c021ab7493
Иван <vanyastepanow@yandex.ru>
2020-10-23 19:54:14 +0300

    Добавляет масштаб и валидацию хэштегов

--- js/main.js
+++ js/main.js
@@ -79,12 +79,12 @@ const getPhotos = function () {
   let photos = [];
   for (let i = 1; i <= PHOTOS_AMOUNT; i++) {
     photos.push(
-        {
-          url: `photos/${i}.jpg`,
-          description: `Фото номер ${i}`,
-          likes: getLikesNumber(),
-          comments: getComments()
-        }
+      {
+        url: `photos/${i}.jpg`,
+        description: `Фото номер ${i}`,
+        likes: getLikesNumber(),
+        comments: getComments()
+      }
     );
   }
   return photos;
@@ -116,7 +116,7 @@ for (let i = 0; i < PHOTOS_AMOUNT; i++) {
 // Приближённая фотография
 
 const mainPicture = document.querySelector(`.big-picture`);
-mainPicture.classList.remove(`hidden`);
+//mainPicture.classList.remove(`hidden`);
 
 const mainPictureImg = mainPicture.querySelector(`.big-picture__img`);
 mainPictureImg.children[0].src = photosList[0].url;
@@ -174,6 +174,7 @@ mainBody.classList.add(`modal-open`);
 const uploadOpen = document.querySelector(`#upload-file`);
 const upload = document.querySelector(`.img-upload__overlay`);
 const uploadClose = upload.querySelector(`#upload-cancel`);
+const uploadHashtag = upload.querySelector(`.text__hashtags`);
 
 const onUploadEscPress = function (evt) {
   if (evt.key === `Escape`) {
@@ -195,7 +196,7 @@ const closeUpload = function () {
   document.removeEventListener(`keydown`, onUploadEscPress);
 };
 
-uploadOpen.addEventListener(`click`, function () {
+uploadOpen.addEventListener(`change`, function () {
   openUpload();
 });
 
@@ -215,9 +216,147 @@ uploadClose.addEventListener(`keydown`, function (evt) {
   }
 });
 
-// Ползунок
+// Хештеги
 
+uploadHashtag.addEventListener(`focus`, function (evt) {
+  document.removeEventListener(`keydown`, onUploadEscPress);
+});
+
+uploadHashtag.addEventListener(`blur`, function (evt) {
+  document.addEventListener(`keydown`, onUploadEscPress);
+});
+
+uploadHashtag.addEventListener(`input`, function (evt) {
+  const hashTags = evt.target.value.toLowerCase().split(` `);
+
+  if (!checkHashtagsLength(hashTags)) {
+    uploadHashtag.setCustomValidity(`Неправильная длина хэштега`);
+  } else if (!isUniTag(hashTags)) {
+    uploadHashtag.setCustomValidity(`Хэштеги должны быть уникальными`);
+  } else if (!isRightTagFormat(hashTags)) {
+    uploadHashtag.setCustomValidity(`Из символов допускается только # в начале тега`);
+  } else if (hashTags.length > 5) {
+    uploadHashtag.setCustomValidity(`Можно указать не больше пяти хэштегов`);
+  } else {
+    uploadHashtag.setCustomValidity(``);
+  }
+});
+
+const isRightTagFormat = function (hashTags) {
+  for (let i = 0; i < hashTags.length; i++) {
+    if (!hashTags[i].match(/^#[0-9a-zа-я]+$/)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const checkHashtagsLength = function (hashTags) {
+  for (let i = 0; i < hashTags.length; i++) {
+    if (hashTags[i].length <= 1 || hashTags[i].length >= 20) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const isUniTag = function (hashTags) {
+  const unicHashtagsList = {};
+  for (let i = 0; i < hashTags.length; i++) {
+    if (hashTags[i] in unicHashtagsList) {
+      return false;
+    } else {
+      unicHashtagsList[hashTags[i]] = true;
+    }
+  }
+
+  return true;
+}
+
+
+
+
+// Редактирование изображения и ограничения, накладываемые на поля: Масштаб
+
+const scaleSmaller = document.querySelector(`.scale__control--smaller`);
+const scaleBigger = document.querySelector(`.scale__control--bigger`);
+const scaleValue = document.querySelector(`.scale__control--value`);
+const uploadPreview = document.querySelector(`.img-upload__preview`);
+const uploadPreviewImage = uploadPreview.querySelector(`img`);
+const scaleValueDefault = 100;
+const scaleValueMin = 25;
+const scaleValueMax = 100;
+scaleValue.value = scaleValueDefault;
+const scaleStep = 25;
+
+scaleSmaller.addEventListener(`click`, function (evt) {
+  if (scaleValue.value > scaleValueMin && scaleValue.value <= scaleValueMax) {
+    evt.preventDefault();
+    scaleValue.value = +scaleValue.value - scaleStep;
+    uploadPreviewImage.style.transform = `scale(${scaleValue.value / 100})`;
+  }
+});
+
+scaleBigger.addEventListener(`click`, function (evt) {
+  if (scaleValue.value >= scaleValueMin && scaleValue.value < scaleValueMax) {
+    evt.preventDefault();
+    scaleValue.value = +scaleValue.value + scaleStep;
+    uploadPreviewImage.style.transform = `scale(${scaleValue.value / 100})`;
+  }
+});
+
+// Редактирование изображения и ограничения, накладываемые на поля: Наложение эффекта на изображение
+
+const effectItems = document.querySelectorAll(`.effects__radio`);
 const saturationToggle = document.querySelector(`.effect-level__pin`);
-saturationToggle.addEventListener(`mouseup`, function () {} )
+const saturationValue = document.querySelector(`.effect-level__value`);
+const saturationSlider = document.querySelector(`.effect-level__line`);
 
-// Следующий шаг
+for (let i = 0; i < effectItems.length; i++) {
+  effectItems[i].addEventListener(`change`, function (evt) {
+    if (evt.target.value === `chrome`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = `effects__preview--chrome`;
+    } else if (evt.target.value === `sepia`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = `effects__preview--sepia`;
+    } else if (evt.target.value === `marvin`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = `effects__preview--marvin`;
+    } else if (evt.target.value === `phobos`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = `effects__preview--phobos`;
+    } else if (evt.target.value === `heat`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = `effects__preview--heat`;
+    } else if (evt.target.value === `none`) {
+      uploadPreviewImage.style.filter = ``;
+      uploadPreviewImage.className = ``;
+    }
+  })
+}
+
+saturationToggle.addEventListener(`mouseup`, function (evt) {
+  const percent = saturationToggle.offsetLeft / saturationSlider.offsetWidth;
+  switch (uploadPreviewImage.className) {
+    case `effects__preview--chrome`:
+      uploadPreviewImage.style.filter = `grayscale(${percent})`;
+      break;
+    case `effects__preview--sepia`:
+      uploadPreviewImage.style.filter = `sepia(${percent})`;
+      break;
+    case `effects__preview--marvin`:
+      uploadPreviewImage.style.filter = `invert(${percent * 100}%)`;
+      break;
+    case `effects__preview--phobos`:
+      uploadPreviewImage.style.filter = `blur(${percent * 3}px)`;
+      break;
+    case `effects__preview--heat`:
+      uploadPreviewImage.style.filter = `brightness(${1 + percent * 2})`;
+      break;
+    default:
+      uploadPreviewImage.style.filter = ``;
+  }
+})
